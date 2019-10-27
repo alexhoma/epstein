3de1c9b564ada7097cef058f72492ad91606fef1
alexhoma <alexcm.14@gmail.com>
2019-10-27 14:13:35 +0100

    docs: adds more books for the example

---
+++ examples/books.js
@@ -0,0 +1,127 @@
+var books = [
+  {
+    title: 'Auberon',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/auberon/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2019/10/Auberon-4rev21-800x1182.jpg',
+    content:
+      'Auberon is one of the first and most important colony worlds in humanitys reach, and the new conquering faction has come to claim it. Governor Rittenaur has come to bring civilization and order to the far outpost and guarantee the wealth and power of the Empire.',
+  },
+  {
+    title: 'Leviathan Wakes',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/leviathan-wakes/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2015/05/Leviathan-Updated-97803161346751.jpg',
+    content:
+      'Two hundred years after migrating into space, mankind is in turmoil. When a reluctant ships captain and washed-up detective find themselves involved in the case of a missing girl, what they discover brings our solar system to the brink of civil war, and exposes the greatest conspiracy in human history.',
+  },
+  {
+    title: 'Calibans War',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/calibans-war/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2015/05/CalibansWar_TPB_2019.jpg',
+    content:
+      'On Ganymede, breadbasket of the outer planets, a Martian marine watches as her platoon is slaughtered by a monstrous supersoldier. On Earth, a high-level politician struggles to prevent interplanetary war from reigniting. And on Venus, an alien protomolecule has overrun the planet, wreaking massive, mysterious changes and threatening to spread out into the solar system.',
+  },
+  {
+    title: 'Abaddons Gate',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/abaddons-gate/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2015/05/AbaddonsGate_TPB_2019.jpg',
+    content:
+      'For generations, the solar system — Mars, the Moon, the Asteroid Belt — was humanitys great frontier. Until now. The alien artifact working through its program under the clouds of Venus has appeared in Uranuss orbit, where it has built a massive gate that leads to a starless dark.',
+  },
+  {
+    title: 'Cibola Burn',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/cibola-burn/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2015/05/Corey_CibolaBurn_TPB.jpg',
+    content:
+      'The gates have opened the way to a thousand new worlds and the rush to colonize has begun. Settlers looking for a new life stream out from humanitys home planets. Ilus, the first human colony on this vast new frontier, is being born in blood and fire.',
+  },
+  {
+    title: 'Nemesis Games',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/nemesis-games/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2015/05/Corey_NemesisGames_HC.jpg',
+    content:
+      'Ships are disappearing without a trace. Private armies are being secretly formed. The sole remaining protomolecule sample is stolen. Terrorist attacks previously considered impossible bring the inner planets to their knees. The sins of the past are returning to exact a terrible price.',
+  },
+  {
+    title: 'Babylons Ashes',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/babylons-ashes/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2016/12/BablyonsAshes.jpg',
+    content:
+      'The Free Navy – a violent group of Belters in black-market military ships – has crippled the Earth and begun a campaign of piracy and violence among the outer planets. The colony ships heading for the thousand new worlds on the far side of the alien ring gates are easy prey, and no single navy remains strong enough to protect them.',
+  },
+  {
+    title: 'The Butcher of Anderson Station',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/the-butcher-of-anderson-station/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2017/01/TheButcherofAndersonStation.jpg',
+    content:
+      'A new story set in the world of the Expanse. One day, Colonel Fred Johnson will be hailed as a hero to the system. One day, he will meet a desperate man in possession of a stolen spaceship and a deadly secret and extend a hand of friendship. But long before he became the leader of the Outer Planets Alliance, Fred Johnson had a very different name. The Butcher of Anderson Station.',
+  },
+  {
+    title: 'Gods of Risk',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/gods-of-risk/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2017/01/GodsofRisk.jpg',
+    content:
+      'As tension between Mars and Earth mounts, and terrorism plagues the Martian city of Londres Nova, sixteen-year-old David Draper is fighting his own lonely war. A gifted chemist vying for a place at the university, David leads a secret life as a manufacturer for a ruthless drug dealer. When his friend Leelee goes missing, leaving signs of the dealers involvement, David takes it upon himself to save her. But first he must shake his aunt Bobbie Draper, an ex-marine who has been set adrift in her own life after a mysterious series of events nobody is talking about.',
+  },
+  {
+    title: 'The Churn',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/churn/',
+    img: 'https://www.jamessacorey.com/wp-content/uploads/2017/01/TheChurn.jpg',
+    content:
+      'Before his trip to the stars, before the Rocinante, Timmy was confined to a Baltimore where crime paid you or killed you. Unless the authorities got to you first. On a future Earth beset by overpopulation, pollution, and poverty, people do what they must to survive. The Churn follows a crime boss named Burton as his organization is threatened by a new private security force tasked with cleaning up the city. When the police start cracking down, Burton and his footsoldiers-loyal lieutenant Erich, former prostitute Lydia, and young enforcer Timmy-become increasingly desperate to find a way out.',
+  },
+  {
+    title: 'The Vital Abyss',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/vital-abyss/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2017/01/VitalAbyss.jpg',
+    content:
+      'Somewhere in the vast expanse of space, a group of prisoners lives in permanent captivity. The only company they have is each other and the Belters who guard them. The only stories they know are the triumphs and crimes that brought them there. The only future they see is an empty life in an enormous room.',
+  },
+  {
+    title: 'Strange Dogs',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/strange-dogs/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2017/06/StrangeDogsCover.jpg',
+    content:
+      'Like many before them, Cara and her family ventured through the gates as scientists and researchers, driven to carve out a new life and uncover the endless possibilities of the unexplored alien worlds now within reach. But soon the soldiers followed and under this new order Cara makes a discovery that will change everything.',
+  },
+  {
+    title: 'Persepolis Rising',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/persepolis-rising/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2017/11/PersepolisRising_TPB_2019.jpg',
+    content:
+      'In the thousand-sun network of humanitys expansion, new colony worlds are struggling to find their way. Every new planet lives on a knife edge between collapse and wonder, and the crew of the aging gunship Rocinante have their hands more than full keeping the fragile peace.',
+  },
+  {
+    title: 'Tiamats Wrath',
+    author: 'James SA Corey',
+    link: 'https://www.jamessacorey.com/books/tiamats-wrath/',
+    img:
+      'https://www.jamessacorey.com/wp-content/uploads/2018/04/TiamatsWrath_HC_2019.jpg',
+    content:
+      'Thirteen hundred gates have opened to solar systems around the galaxy. But as humanity builds its interstellar empire in the alien ruins, the mysteries and threats grow deeper. In the dead systems where gates lead to stranger things than alien planets, Elvi Okoye begins a desperate search to discover the nature of a genocide that happened before the first human beings existed, and to find weapons to fight a war against forces at the edge of the imaginable. But the price of that knowledge may be higher than she can pay.',
+  },
+];
