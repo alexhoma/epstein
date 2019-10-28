22770876f14a4293fadcc0dbbb731d99fa216346
alexhoma <alexcm.14@gmail.com>
2019-10-28 23:51:06 +0100

    chore: minify umd distribution file

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,50 @@
 import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    { file: 'dist/epstein.js', format: 'cjs' },
-    { file: 'dist/epstein.es.js', format: 'es' },
-    { file: 'dist/epstein.umd.js', format: 'umd', name: 'epstein' },
-  ],
-  plugins: [
-    resolve({
-      extensions: ['.js'],
-    }),
-  ],
-};
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/epstein.js',
+      format: 'cjs',
+    },
+    plugins: [
+      resolve({
+        extensions: ['.js'],
+      }),
+    ],
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/epstein.es.js',
+      format: 'es',
+    },
+    plugins: [
+      resolve({
+        extensions: ['.js'],
+      }),
+    ],
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/epstein.umd.js',
+      format: 'umd',
+      name: 'epstein',
+    },
+    plugins: [
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false,
+        },
+      }),
+      resolve({
+        extensions: ['.js'],
+      }),
+    ],
+  },
+];
